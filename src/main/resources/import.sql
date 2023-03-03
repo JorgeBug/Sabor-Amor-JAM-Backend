b84164800b794d25c267436add10c8e5c9ac1454
Jose <jose.mar.123@hotmail.com>
2023-03-03 00:34:38 -0600

    Add usuario insertion queries to import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,6 +4,18 @@ INSERT INTO empleado(nombre, apellido, puesto, fecha_nacimiento, salario) VALUES
 INSERT INTO empleado(nombre, apellido, puesto, fecha_nacimiento, salario) VALUES ("Mariana", "García", "Empaquetadora", "1993-09-15", "8000.00")
 INSERT INTO empleado(nombre, apellido, puesto, fecha_nacimiento, salario) VALUES ("Clemente", "Ramírez", "Intendente", "1971-11-01", "7000.00")
 
-INSERT INTO usuario(nombre, apellido, email, telefono,contrasenia ) VALUES("José", "Fernández", "masterdelingles@generation.mx", "55 54-673823","password");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("José", "Fernández", "masterdelingles@generation.mx", "55 54-673823","123");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("Juan", "Herrera", "juanverdadero@generation.mx", "2222893821","123");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("Karla", "Arenas", "tapatia@generation.mx", "3336256243","123");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("Luis", "Soto", "emo@generation.mx", "3194637593","123");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("Francisco", "Betancourt", "pax@generation.mx", "5544928374","123");
+INSERT INTO usuario(nombre, apellido, email, telefono, contrasenia) VALUES ("José", "Guevara", "guevi@buap.mx", "2221849572","123");
+
+
+
+
+
+
+
 INSERT INTO carrito(fk_id_usuario) VALUES ("1");
 
